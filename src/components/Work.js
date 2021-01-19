0d1ff1d62514c72c2d37cdaedec076560d9a3d95
hiro-bata <kawa.060313@gmail.com>
2021-01-20 07:01:25 +0900

    add_work

--- src/components/Work.js
+++ src/components/Work.js
@@ -5,6 +5,7 @@ import TimeManage from "../images/time-manage.jpeg"
 import ReactApp from "../images/schedule.png"
 import Portfolio from "../images/portfolio.png"
 import Judge from "../images/judge.png"
+import Dog from "../images/dog.png"
 import Header from "./Header"
 
 
@@ -20,6 +21,7 @@ class Work extends Component {
                     <Card title="Portfolio" desc="Reactで実装したPortfolioサイト。" link="https://portfolio-e9886.web.app/" img={Portfolio}/>
                     <Card title="Time-manage" desc="実行したアクションと時間を記録することで、1日のうち、何に何時間かけているかを可視化し、時間の有効活用に役立てるRailsアプリ。AWS上でデプロイ。" link="https://time-manage-0603.herokuapp.com/" img={TimeManage}/>
                     <Card title="Judge" desc="選択肢を入力し、Judgeボタンを押すと、選択肢の中からランダムで1つが選ばれるReactアプリ。判断に迷ったら使用。" link="https://judge-69905.web.app/" img={Judge}/>
+                    <Card title="Dog-image" desc="犬の画像をAPIで引っ張って表示するReactアプリ。APIの練習目的で作成。リロードするごとに画像が変更される。セレクトボックスで柴犬と秋田犬の画像切り替えが可能。" link="https://dog-image.web.app/" img={Dog}/>
             </div>
         </div>
     );
